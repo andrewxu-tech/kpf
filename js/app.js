19a284020e14280fbb975fc6470cb24c66142eb6
Andrew Xu <alexandrew94@hotmail.com>
2021-04-20 22:41:44 +0100

    First step for initial page

--- js/app.js
+++ js/app.js
@@ -1,5 +1,34 @@
-const giveAlert = () => {
-  alert('blah blah');
-};
+const bubblesContents = [
+  {
+    name: 'Resilience'
+  },
+  {
+    name: 'Community'
+  },
+  {
+    name: 'Sustainability'
+  },
+  {
+    name: 'Wellness'
+  }
+];
 
-document.getElementsByTagName('h1')[0].addEventListener('click', giveAlert);
+bubblesContents.forEach(element => {
+  const bubbleWrapper = document.createElement('div');
+  bubbleWrapper.classList.add('selectable-bubble-wrapper');
+
+  const text = document.createElement('text');
+  text.innerHTML = element.name;
+  text.id = element.name;
+  text.classList.add('bubble-text');
+  bubbleWrapper.appendChild(text);
+
+  const bubbleCircle = document.createElement('div');
+  bubbleCircle.classList.add('selectable-bubble-circle');
+  bubbleWrapper.appendChild(bubbleCircle);
+
+  document.getElementsByTagName('main')[0].appendChild(bubbleWrapper);
+
+  const circleType = new CircleType(document.getElementById(element.name)); // eslint-disable-line
+  circleType.radius(200).dir();
+});
